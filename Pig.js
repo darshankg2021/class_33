7844b8affa86473efa290ada94241b1ace458c03
darshankg2020 <darshankumarkg2020@gmail.com>
2021-09-16 15:45:27 +0530

    added a score function

--- Pig.js
+++ Pig.js
@@ -5,6 +5,8 @@ class Pig extends BaseClass {
     this.Visiblity = 255;
   }
 
+  
+
  display(){
    //console.log(this.body.speed);
    if(this.body.speed < 3){
@@ -17,9 +19,13 @@ class Pig extends BaseClass {
      tint(255,this.Visiblity);
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
      pop();
-   }
-   
+   }   
  }
+  score(){
+    if(this.Visiblity < 0 && this.Visiblity > -1000){
+      score++;
+    }
+  }
 
 
 
